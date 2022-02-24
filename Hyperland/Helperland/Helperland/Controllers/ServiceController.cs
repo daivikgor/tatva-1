58e1d1ad91be44f6e4cba16cb2927790a763fc14
Daivik Rajgor <daivikgor52@gmail.com>
2022-02-24 18:29:02 +0530

    DB schema update

--- Hyperland/Helperland/Helperland/Controllers/ServiceController.cs
+++ Hyperland/Helperland/Helperland/Controllers/ServiceController.cs
@@ -1,16 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Helperland.Data;
+using Helperland.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace Helperland.Controllers
 {
     public class ServiceController : Controller
     {
+        readonly HelperlandContext dc = new HelperlandContext();
         public IActionResult bookservice()
         {
             return View();
+        }
+
+        //[HttpGet]
+        //public IActionResult SetupService()
+        //{
+        //    return View();
+        //}
+        [HttpGet]
+        public string searchzipcode(string zipcode)
+        {
+            if (HttpContext.Session.GetString("username") != null)
+            {
+                var zipcode1 = dc.Zipcodes.Where(z => z.ZipcodeValue == zipcode).FirstOrDefault();
+                var zipcode2 = dc.Users.Where(z => z.ZipCode == zipcode && z.UserTypeId == 2).FirstOrDefault();
+
+                if (zipcode1 != null)
+                {
+                    return "true";
+                }
+                else
+                {
+                    return "false" ;
+
+                }
+                
+
+            }
+            else
+            {
+                return "login";
+            }
         }
     }
 }
--- Hyperland/Helperland/Helperland/ViewModel/ServiceViewModel.cs
+++ Hyperland/Helperland/Helperland/ViewModel/ServiceViewModel.cs
@@ -8,39 +8,9 @@ namespace Helperland.ViewModel
 {
     public class ServiceViewModel
     {
-        [Required(ErrorMessage = "First Name is required")]
-        public string FirstName { get; set; }
-        [Required(ErrorMessage = "Last Name is required")]
-        public string LastName { get; set; }
-        [Required(ErrorMessage = "Email is required")]
-        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
-        ErrorMessage = "Invalid email format")]
-        public string Email { get; set; }
-        [Required(ErrorMessage = "Password is required")]
-        [DataType(DataType.Password)]
-        [MinLength(8, ErrorMessage = "password must be more than eigth characters")]
-        public string Password { get; set; }
-        [Required(ErrorMessage = "mobile number is required")]
-        public string Mobile { get; set; }
+        public int Id { get; set; }
 
-        [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage = "password and confirm password does not match")]
-        public string Confirmpassword { get; set; }
-        public int? UserTypeId { get; set; }
-        public bool? IsRegisteredUser { get; set; }
-        public bool? WorksWithPets { get; set; }
-        public int? LanguageId { get; set; }
-        public int? NationalityId { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime? ModifiedDate { get; set; }
-        public int? ModifiedBy { get; set; }
-        public bool? IsApproved { get; set; }
-        public bool? IsActive { get; set; }
-        public bool? IsDeleted { get; set; }
-
-        [Required(ErrorMessage = "Required")]
-        public bool Terms { get; set; }
-        [Required(ErrorMessage = "Required")]
-        public bool Privacy { get; set; }
+        public string ZipcodeValue { get; set; }
+        public int CityId { get; set; }
     }
 }
