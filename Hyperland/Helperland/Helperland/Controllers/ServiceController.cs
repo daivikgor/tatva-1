315f07c8398c664b4eadca7f7ef10931f8923ff5
Daivik Rajgor <daivikgor52@gmail.com>
2022-03-02 19:06:36 +0530

    changes in book service

--- Hyperland/Helperland/Helperland/Controllers/ServiceController.cs
+++ Hyperland/Helperland/Helperland/Controllers/ServiceController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net;
+using Helperland.ViewModel;
 
 namespace Helperland.Controllers
 {
@@ -37,10 +38,10 @@ namespace Helperland.Controllers
                 }
                 else
                 {
-                    return "false" ;
+                    return "false";
 
                 }
-                
+
 
             }
             else
@@ -48,5 +49,125 @@ namespace Helperland.Controllers
                 return "login";
             }
         }
+        [HttpGet]
+        public IActionResult schedule()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult schedule(ServiceViewModel model)
+        {
+            var username = HttpContext.Session.GetString("username");
+            var id = dc.Users
+                .Where(a => a.Email == username)
+                .Select(a => a.UserId).FirstOrDefault();
+
+            var postalcode = dc.UserAddresses
+                .Where(a => a.UserId == id)
+                .Select(a => a.PostalCode).FirstOrDefault();
+
+            ViewBag.name = id;
+            var extra_hrs = (model.ExtraHours) / 2;
+            var cost = (decimal)(20 * (model.ServiceHours));
+
+            ServiceRequest Request = new ServiceRequest
+            {
+                UserId = id,
+                ServiceStartDate = model.ServiceStartDate,
+                ServiceHourlyRate = 20,
+                ServiceHours = model.ServiceHours,
+                ExtraHours = extra_hrs,
+                Comments = model.Comments,
+                HasPets = model.HasPets,
+                ZipCode = postalcode,
+                SubTotal = cost,
+                TotalCost = ((decimal)(20 * (extra_hrs))) + cost,
+                ModifiedDate = model.ServiceStartDate,
+                Distance = 1
+            };
+            dc.Add(Request);
+            dc.SaveChanges();
+            return View("Service");
+        }
+        [HttpGet]
+        public IActionResult Details()
+        {
+            var username = HttpContext.Session.GetString("username");
+            var id = dc.Users
+                .Where(a => a.Email == username)
+                .Select(a => a.UserId)
+                .FirstOrDefault();
+            ViewBag.name = username;
+
+            List<UserAddress> add = dc.UserAddresses
+                .Where(a => a.UserId == id).ToList();
+            return View(add);
+        }
+
+        [HttpPost]
+        public IActionResult Details(ServiceViewModel model, string Address_radio, string action)
+        {
+            var username = HttpContext.Session.GetString("username");
+            var id = dc.Users
+                .Where(a => a.Email == username)
+                .Select(a => a.UserId)
+                .FirstOrDefault();
+
+            var serviceReqId = dc.ServiceRequests
+                   .Where(a => a.UserId == id)
+                   .OrderByDescending(a => a)
+                   .Select(a => a.ServiceRequestId)
+                   .FirstOrDefault();
+
+            if (action == "addSerAdd")
+            {
+                int Address_id = int.Parse(Address_radio);
+
+                UserAddress newUserAdd = dc.UserAddresses
+                                         .Where(a => a.AddressId == Address_id)
+                                         .FirstOrDefault();
+
+                ServiceRequestAddress serviceRequestAddress = new ServiceRequestAddress
+                {
+                    ServiceRequestId = serviceReqId,
+                    AddressLine1 = newUserAdd.AddressLine1,
+                    AddressLine2 = newUserAdd.AddressLine2,
+                    City = newUserAdd.City,
+                    State = newUserAdd.State,
+                    PostalCode = newUserAdd.PostalCode,
+                    Mobile = newUserAdd.Mobile
+                };
+                dc.Add(serviceRequestAddress);
+                dc.SaveChanges();
+            }
+
+            else
+            {
+
+                UserAddress userAddress = new UserAddress
+                {
+                    UserId = id,
+                    AddressLine1 = model.AddressLine1,
+                    AddressLine2 = model.AddressLine2,
+                    City = model.City,
+                    State = model.State,
+                    PostalCode = model.PostalCode,
+                    Mobile = model.Mobile,
+                    IsDefault = true,
+                    IsDeleted = true
+                };
+                dc.Add(userAddress);
+                dc.SaveChanges();
+            }
+
+            ViewBag.Msg = serviceReqId;
+
+
+            return View("bookservice");
+        }
+
+
     }
 }
+
